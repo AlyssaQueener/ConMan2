8845e6b3702b7f9a4bdc0722e7a8258cf7369f0f
Julian Gerstner <julian.gerstner@tum.de>
2025-06-13 17:41:29 +0200

    adjusted timestamps

--- src/script_runDiff.py
+++ src/script_runDiff.py
@@ -1,8 +1,8 @@
 from neo4j_core.neo4j_connection import Neo4jConnection
 from graph_diff.graph_diff import GraphDiff
 
-timestamp_init = "1"
-timestamp_updt = "2"
+timestamp_init = "0"
+timestamp_updt = "1"
 
 db = Neo4jConnection(username="neo4j", password="password", hostname="localhost", port=7687)
 graph_diff = GraphDiff()
