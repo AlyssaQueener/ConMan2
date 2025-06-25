be09a08e14d572d3b88d83774c00e21597707d1f
Julian Gerstner <julian.gerstner@tum.de>
2025-06-25 14:51:53 +0200

    added global unique paths list and populating gluing pattern relations with global unique path to context

--- src/graph_diff/graph_diff.py
+++ src/graph_diff/graph_diff.py
@@ -9,6 +9,7 @@ import json
 class GraphDiff:
 
     node_matching_table = []
+    unique_paths = {}
 
     ########################
     ### Helper Functions ###
@@ -17,17 +18,19 @@ class GraphDiff:
     def create_equivalence_relations_primary(self, equiv_node_init, equiv_node_updt, unique_path_init=[], unique_path_updt=[]):
         # Reset unique path if the current node has a unique id
         if type(equiv_node_init) == PrimaryNode:
-            unique_path_init = [equiv_node_init.GlobalId]
+            unique_path_init = [{"primary_node": equiv_node_init.GlobalId}]
+            self.unique_paths[equiv_node_init.element_id] = [{"primary_node": equiv_node_init.GlobalId}]
         if type(equiv_node_updt) == PrimaryNode:
-            unique_path_updt = [equiv_node_updt.GlobalId]
+            unique_path_updt = [{"primary_node": equiv_node_updt.GlobalId}]
+            self.unique_paths[equiv_node_updt.element_id] = [{"primary_node": equiv_node_updt.GlobalId}]
 
         for child_init in equiv_node_init.relation_to.all():
             rel_init = equiv_node_init.relation_to.relationship(child_init)
-            new_path_init = unique_path_init + [{"node": child_init, "rel_type": rel_init.rel_type, "list_index": rel_init.list_index, "EntityType": child_init.EntityType}]
+            new_path_init = unique_path_init + [{"rel_type": rel_init.rel_type, "list_index": rel_init.list_index, "EntityType": child_init.EntityType}]
             
             for child_updt in equiv_node_updt.relation_to.all():
                 rel_updt = equiv_node_updt.relation_to.relationship(child_updt)
-                new_path_updt = unique_path_updt + [{"node": child_updt, "rel_type": rel_updt.rel_type, "list_index": rel_updt.list_index, "EntityType": child_updt.EntityType}]
+                new_path_updt = unique_path_updt + [{"rel_type": rel_updt.rel_type, "list_index": rel_updt.list_index, "EntityType": child_updt.EntityType}]
                 
                 if (
                     rel_init.rel_type == rel_updt.rel_type
@@ -38,6 +41,8 @@ class GraphDiff:
                 ):
                     child_init.equivalent_to.connect(child_updt)
                     self.node_matching_table.append([new_path_init.copy(), new_path_updt.copy()])
+                    self.unique_paths[child_init.element_id] = json.dumps(new_path_init.copy())
+                    self.unique_paths[child_updt.element_id] = json.dumps(new_path_updt.copy())
                     self.create_equivalence_relations_primary(child_init, child_updt, new_path_init, new_path_updt)
 
 
@@ -82,10 +87,12 @@ class GraphDiff:
                     if relation_to:
                         if relation_to.gluing_id is None:
                             relation_to.gluing_id = pushout_id
+                            relation_to.context_node = self.unique_paths[adjacent.element_id]
                             relation_to.save()
                     if relation_from:
                         if relation_from.gluing_id is None:
                             relation_from.gluing_id = pushout_id
+                            relation_to.context_node = self.unique_paths[adjacent.element_id]
                             relation_from.save()
                     
                     
