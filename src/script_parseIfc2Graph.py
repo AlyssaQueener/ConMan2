daa15a1a536b0d9c8ee6122502f7149c20f3d7ba
Julian Gerstner <julian.gerstner@tum.de>
2025-06-27 15:02:06 +0200

    test files changed

--- src/script_parseIfc2Graph.py
+++ src/script_parseIfc2Graph.py
@@ -4,10 +4,9 @@ from neo4j_core.neo4j_connection import Neo4jConnection
 from ifc_graph_interface.IfcGraphInterface import IfcGraphInterface
 
 paths = [
-    #'00_sampleData/IFC_stepP21/DepMod2025/2025-DepMod2-ARC-Model_v1.ifc',
-    '00_sampleData/IFC_stepP21/DepMod2025/2025-DepMod2HVAC-Model-v1.ifc',
-    '00_sampleData/IFC_stepP21/DepMod2025/2025-DepMod2HVAC-Model-v2.ifc',
-    #'00_sampleData/IFC_stepP21/DepMod2025/2025-DepMod2HVAC-Model-v3.ifc',
+    # "./00_sampleData/IFC_stepP21/diss-casestudy/ARC-v1-purified.ifc",
+    "./00_sampleData/IFC_stepP21/diss-casestudy/ARC-v2-purified.ifc",
+    "./00_sampleData/IFC_stepP21/diss-casestudy/ARC-v3-purified.ifc"
 ]
 
 db = Neo4jConnection(username="neo4j", password="password", hostname="localhost", port=7687)
