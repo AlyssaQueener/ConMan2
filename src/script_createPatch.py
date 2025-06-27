34601595da2c3381401bd88e8ee14b2f1e3bb8ec
Julian Gerstner <julian.gerstner@tum.de>
2025-06-27 15:39:36 +0200

    migrated patch operations to new dir

---
+++ src/script_createPatch.py
@@ -0,0 +1,14 @@
+from neo4j_core.neo4j_connection import Neo4jConnection
+from graph_patch.graph_patch import GraphPatch
+
+# For simplicity and testing:
+import script_parseIfc2Graph
+import script_runDiff
+
+timestamp_init = "0"
+timestamp_updt = "1"
+
+db = Neo4jConnection(username="neo4j", password="password", hostname="localhost", port=7687)
+graph_patch = GraphPatch()
+
+graph_patch.create_patch(timestamp_init, timestamp_updt)
