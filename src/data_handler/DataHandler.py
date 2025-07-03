b667f0a4a3c013476f58720165351ca6afa3c8d5
Julian Gerstner <julian.gerstner@tum.de>
2025-07-03 08:49:09 +0200

    added path hasher for stable json parsing of unique paths

---
+++ src/data_handler/DataHandler.py
@@ -0,0 +1,11 @@
+import json
+import hashlib
+
+
+
+class DataHandler:
+
+    def path_to_hash(path):
+        path_string = json.dumps(path)
+        path_hash = hashlib.sha256(path_string.encode('utf-8')).hexdigest()
+        return path_hash
