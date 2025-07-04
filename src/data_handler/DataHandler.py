785726022af579024ed2e64c0dcd25629332a2a3
Julian Gerstner <julian.gerstner@tum.de>
2025-07-04 09:41:46 +0200

    switched from sha256 to sha1 for speed

--- src/data_handler/DataHandler.py
+++ src/data_handler/DataHandler.py
@@ -7,5 +7,5 @@ class DataHandler:
 
     def path_to_hash(path):
         path_string = json.dumps(path)
-        path_hash = hashlib.sha256(path_string.encode('utf-8')).hexdigest()
+        path_hash = hashlib.sha1(path_string.encode('utf-8')).hexdigest()
         return path_hash
