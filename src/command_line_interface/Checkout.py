9d0dfe939f324bebe6613826782dc58f314354d3
ge73hig <julian.gerstner@tum.de>
2025-09-03 13:48:13 +0200

    placeholder, tobe implemented

--- src/command_line_interface/Checkout.py
+++ src/command_line_interface/Checkout.py
@@ -1,40 +1,33 @@
-from graph_diff.GraphDiff import GraphDiff
-from graph_patch.GraphPatch import GraphPatch
-from neo4j_core.neo4j_connection import Neo4jConnection
 from version_timeline.VersionTimeline import VersionTimeline
+from neo4j_core.neo4j_connection import Neo4jConnection
+from ifc_graph_interface.IfcGraphInterface import IfcGraphInterface
 
 version_timeline = VersionTimeline()
+Neo4jConnection(username="neo4j", password="password", hostname="localhost", port=7687)
 
-def checkout(timestamp_init: str, timestamp_updt: str):
+def checkout(project_id: str, branch_init: str, branch_updt: str, timestamp_updt: str):
+    
+    version_timeline.checkout(project_id, branch_init, branch_updt, timestamp_updt)
 
-    Neo4jConnection(username="neo4j", password="password", hostname="localhost", port=7687)
 
-    if version_timeline.timeline == {}:
-        print("No timeline found. Please commit at least one version before checking out.")
-        return None
-    else:
-        if version_timeline.get_project_id_from_timestamp(timestamp_init):
-            project_id = version_timeline.get_project_id_from_timestamp(timestamp_init)
-            start_index = version_timeline.timeline[project_id].index(timestamp_init)
-            stop_index = version_timeline.timeline[project_id].index(timestamp_updt)
-            if start_index > stop_index:
-                step = -1
-            else:
-                step = 1
-            for i in range(start_index, stop_index, step):
-                if i == stop_index:
-                    break
-                if start_index > stop_index:
-                    ts_init = version_timeline.timeline[project_id][i + step]
-                    ts_updt = version_timeline.timeline[project_id][i]
-                    graph_patch = GraphPatch(ts_updt, ts_init)
-                    print(f"Applying patch from {ts_updt} to {ts_init}.")
-                    graph_patch.load_patch_from_file(path_sema=f"./patch_data/Patch_Sema_{ts_init}_{ts_updt}.json", path_topo=f"./patch_data/Patch_Topo_{ts_init}_{ts_updt}.json"                                       )
-                    graph_patch.apply_patch(ts_updt, ts_init)
-                else:
-                    ts_init = version_timeline.timeline[project_id][i]
-                    ts_updt = version_timeline.timeline[project_id][i + step]
-                    graph_patch = GraphPatch(ts_init, ts_updt)
-                    print(f"Applying patch from {ts_init} to {ts_updt}.")
-                    graph_patch.load_patch_from_file(path_sema=f"./patch_data/Patch_Sema_{ts_init}_{ts_updt}.json", path_topo=f"./patch_data/Patch_Topo_{ts_init}_{ts_updt}.json"                                       )
-                    graph_patch.apply_patch(ts_init, ts_updt)
+    # if start_index > stop_index:
+    #     step = -1
+    # else:
+    #     step = 1
+    # for i in range(start_index, stop_index, step):
+    #     if i == stop_index:
+    #         break
+    #     if start_index > stop_index:
+    #         ts_init = self.timeline[project_id]["branches"][branch]["timestamps"][i + step]
+    #         ts_updt = self.timeline[project_id]["branches"][branch]["timestamps"][i]
+    #         graph_patch = GraphPatch(ts_updt, ts_init)
+    #         print(f"Applying patch from {ts_updt} to {ts_init}.")
+    #         graph_patch.load_patch_from_file(path_sema=f"./patch_data/Patch_Sema_{ts_init}_{ts_updt}.json", path_topo=f"./patch_data/Patch_Topo_{ts_init}_{ts_updt}.json"                                       )
+    #         graph_patch.apply_patch(ts_updt, ts_init)
+    #     else:
+    #         ts_init = self.timeline[project_id]["branches"][branch]["timestamps"][i]
+    #         ts_updt = self.timeline[project_id]["branches"][branch]["timestamps"][i + step]
+    #         graph_patch = GraphPatch(ts_init, ts_updt)
+    #         print(f"Applying patch from {ts_init} to {ts_updt}.")
+    #         graph_patch.load_patch_from_file(path_sema=f"./patch_data/Patch_Sema_{ts_init}_{ts_updt}.json", path_topo=f"./patch_data/Patch_Topo_{ts_init}_{ts_updt}.json"                                       )
+    #         graph_patch.apply_patch(ts_init, ts_updt)
